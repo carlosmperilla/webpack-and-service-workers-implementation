48a893d55b7239acef1d342921962ca77ef37689
Carlos <carlosmanueldu@gmail.com>
2023-01-09 21:22:18 -0300

    Implementing service workers

--- assets/js/entry.js
+++ assets/js/entry.js
@@ -2,3 +2,6 @@ import '../css/app.scss';
 
 import './slider/sliderDOM';
 import './menu';
+
+if (navigator.serviceWorker)
+    navigator.serviceWorker.register('../sw.js');
---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'VIDEOGAME-STORE_CACHE-v1';
+importScripts('./filesList.js');
+
+self.addEventListener('install', () => {
+    
+    // Guardar archivos iniciales.
+    caches.open(CACHE_NAME).then((cache) => {
+        cache.addAll(getFilesList());
+    })
+})
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            let promises = cacheNames.map(cacheName => {
+                if (CACHE_NAME !== cacheName) return caches.delete(cacheName);
+            });
+
+            return Promise.all(promises);
+        })
+    );
+})
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then( (response) => {
+            return searchInCacheOrMakeRequest(e.request);
+        }).catch((err) => {
+            if (e.request.mode == 'navigate')
+                return caches.match(e.request);
+        })
+    );
+})
+
+async function searchInCacheOrMakeRequest(request) {
+    const cachePromise = caches.open(CACHE_NAME);
+    const matchPromise = cachePromise.then((cache) => {
+        return cache.match(request);
+    })
+
+    return Promise.all([cachePromise, matchPromise])
+        .then(([cache, cacheResponse]) => {
+            const fetchPromise = fetch(request).then((fetchResponse) => {
+                cache.put(request, fetchResponse.clone());
+                return fetchResponse;
+            })
+            return cacheResponse || fetchPromise;
+        })
+}
